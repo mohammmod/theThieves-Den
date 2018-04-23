904b617638ed18e3376d9b44f58d9d2278f9d804
mohammmod <mohammad.sawas2@outlook.com>
2018-04-23 15:41:36 +0200

    initilaize project

---
+++ src/main/java/at/refugeescode/thethievesden/ThethievesdenApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.thethievesden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThethievesdenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThethievesdenApplication.class, args);
+	}
+}
